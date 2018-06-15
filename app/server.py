4a4504725446865178f7f60774cd3b14862547c2
Terry Chia <terrycwk1994@gmail.com>
2018-06-15 21:29:20 +0800

    Add API endpoint for OWASP Dependency Check scanner.

--- app/server.py
+++ app/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 from app import utils
+from app.scanners import owasp
 
 server = Flask(__name__)
 
@@ -13,3 +14,17 @@ def hello():
 @server.route("/efda/list")
 def list_efda():
     return jsonify(utils.find_efda_projects("efda"))
+
+
+@server.route("/scan/owasp", methods=["POST"])
+def scan_owasp():
+    target = request.form["target"]
+    project_paths = utils.find_efda_projects("efda")
+
+    if target == "all":
+        # TODO
+        return None
+    elif target not in project_paths:
+        abort(404)
+    else:
+        return jsonify(owasp.scan(target))
