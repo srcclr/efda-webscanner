ff7f7335f8ef16f264989b011017120bbf5c927f
Terry Chia <terrycwk1994@gmail.com>
2018-07-07 15:25:39 +0800

    Apply onChange to all checkboxes.

--- ui/src/Home.js
+++ ui/src/Home.js
@@ -46,55 +46,55 @@ class Home extends React.Component {
                     <div> {/*  Java - Maven Projects */ }
                         <Collapsible trigger="Maven" className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value="maven-1" onChange={this.onChange.bind(this)}/>Dependencies Exclusion</label>
+                                <label><input type="checkbox" value="efda/java/maven/exclusion-support" onChange={this.onChange.bind(this)}/>exclusion-support</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Interpolated Variables</label>
+                                <label><input type="checkbox" value="efda/java/maven/interpolated-variables" onChange={this.onChange.bind(this)}/>interpolated-variables</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Project Aggregation</label>
+                                <label><input type="checkbox" value="efda/java/maven/project-aggregation" onChange={this.onChange.bind(this)}/>project-aggregation</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Project Inheritance</label>
+                                <label><input type="checkbox" value="efda/java/maven/project-inheritance" onChange={this.onChange.bind(this)}/>project-inheritance</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Scopes</label>
+                                <label><input type="checkbox" value="efda/java/maven/scopes" onChange={this.onChange.bind(this)}/>scopes</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Version Range</label>
+                                <label><input type="checkbox" value="efda/java/maven/version-range" onChange={this.onChange.bind(this)}/>version-range</label>
                             </div>
                         </Collapsible >
                     </div>
                     <div> {/* Java - Ant Projects */ }
                         <Collapsible trigger="Ant " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With Apachle Ivy</label>
+                                <label><input type="checkbox" value="efda/java/ant/with-ivy" onChange={this.onChange.bind(this)}/>with-ivy</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Without Apachle Ivy</label>
+                                <label><input type="checkbox" value="efda/java/ant/without-ivy" onChange={this.onChange.bind(this)}/>without-ivy</label>
                             </div>
                         </Collapsible>
                     </div>
                     <div> {/* Java - Gradle Projects */ }
                         <Collapsible trigger="Gradle " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Multi Modules</label>
+                                <label><input type="checkbox" value="efda/java/gradle/gradle-scopes" onChange={this.onChange.bind(this)}/>gradle-scopes</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Scopes</label>
+                                <label><input type="checkbox" value="efda/java/gradle/multi-modules" onChange={this.onChange.bind(this)}/>multi-modules</label>
                             </div>
                         </Collapsible>
                     </div>
-                    <div> {/* Java - None Projects */ }
-                        <Collapsible trigger="None " className="subtitle">
+                    <div> {/* Java - Jars Projects */ }
+                        <Collapsible trigger="Jars" className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Original Third-Party Jars</label>
+                                <label><input type="checkbox" value="efda/java/jars/fat-jars" onChange={this.onChange.bind(this)}/>far-jars</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Fat Jars</label>
+                                <label><input type="checkbox" value="efda/java/jars/original-jars" onChange={this.onChange.bind(this)}/>original-jars</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Recompiled Jars Matching</label>
+                                <label><input type="checkbox" value="efda/java/jars/recompiled-jars" onChange={this.onChange.bind(this)}/>recompiled-jars</label>
                             </div>
                         </Collapsible>
                     </div>
@@ -104,10 +104,17 @@ class Home extends React.Component {
                     <div> {/* Python - Pip Projects */ }
                         <Collapsible trigger="Pip " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With requirements.txt</label>
+                                <label><input type="checkbox" value="efda/python/pip/with-requirements-file" onChange={this.onChange.bind(this)}/>with-requirements-file</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With setup.py</label>
+                                <label><input type="checkbox" value="efda/python/pip/with-setup-dot-py" onChange={this.onChange.bind(this)}/>with-setup-dot-py</label>
+                            </div>
+                        </Collapsible>
+                    </div>
+                    <div> {/* Python - Pipenv Projects */ }
+                        <Collapsible trigger="Pipenv " className="subtitle">
+                            <div class="checkbox">
+                                <label><input type="checkbox" value="efda/python/pipenv/with-pipfile-dot-lock" onChange={this.onChange.bind(this)}/>with-pipfile-dot-lock</label>
                             </div>
                         </Collapsible>
                     </div>
@@ -117,33 +124,33 @@ class Home extends React.Component {
                     <div> {/* Javascript - NPM Projects */ }
                         <Collapsible trigger="NPM " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With-package.json only</label>
+                                <label><input type="checkbox" value="efda/javascript/npm/npm-package-lock" onChange={this.onChange.bind(this)}/>npm-package-lock</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Prod & Dev Dependencies</label>
+                                <label><input type="checkbox" value="efda/javascript/npm/npm-shrinkwrap" onChange={this.onChange.bind(this)}/>npm-shrinkwrap</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With npm-shrinkwrap</label>
+                                <label><input type="checkbox" value="efda/javascript/npm/npm-version-ranges" onChange={this.onChange.bind(this)}/>npm-version-ranges</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Version Ranges</label>
+                                <label><input type="checkbox" value="efda/javascript/npm/npm-with-dev-dependencies" onChange={this.onChange.bind(this)}/>npm-with-dev-dependencies</label>
                             </div>
                         </Collapsible>
                     </div>
                     <div>  {/* Javascript - Bower Projects */ }
                         <Collapsible trigger="Bower " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Version Ranges</label>
+                                <label><input type="checkbox" value="efda/javascript/bower/bower-version-ranges" onChange={this.onChange.bind(this)}/>bower-version-ranges</label>
                             </div>
                         </Collapsible>
                     </div>
                     <div>  {/* Javascript - Yarn Projects */ }
                         <Collapsible trigger="Yarn " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With yarn.lock</label>
+                                <label><input type="checkbox" value="efda/javascript/yarn/yarn-lock-with-dev-dependencies" onChange={this.onChange.bind(this)}/>yarn-lock-with-dev-dependencies</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Scopes</label>
+                                <label><input type="checkbox" value="efda/javascript/yarn/yarn-scopes" onChange={this.onChange.bind(this)}/>yarn-scopes</label>
                             </div>
                         </Collapsible>
                     </div>
@@ -153,13 +160,13 @@ class Home extends React.Component {
                     <div>  {/* Ruby - Bundler Projects */ }
                         <Collapsible trigger="Bundler " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Version Ranges</label>
+                                <label><input type="checkbox" value="efda/ruby/bundler/version-ranges" onChange={this.onChange.bind(this)}/>version-ranges</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Groups</label>
+                                <label><input type="checkbox" value="efda/ruby/bundler/with-Gemfile.lock" onChange={this.onChange.bind(this)}/>with-Gemfile.lock</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Only Gemfile.lock</label>
+                                <label><input type="checkbox" value="efda/ruby/bundler/with-dev-dependencies" onChange={this.onChange.bind(this)}/>with-dev-dependencies</label>
                             </div>
                         </Collapsible>
                     </div>
@@ -172,10 +179,10 @@ class Home extends React.Component {
                     <div>  {/* Objective-C - CoacoaPods Projects */ }
                         <Collapsible trigger="CoacoaPods " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Only Podfile</label>
+                                <label><input type="checkbox" value="efda/objective-c/cocoapods/only-podfile" onChange={this.onChange.bind(this)}/>only-podfile</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With Podfile.lock </label>
+                                <label><input type="checkbox" value="efda/objective-c/cocoapods/with-podfile.lock" onChange={this.onChange.bind(this)}/>with-podfile.lock</label>
                             </div>
                         </Collapsible>
                     </div>
@@ -185,13 +192,13 @@ class Home extends React.Component {
                     <div>  {/* PHP - Composer Projects */ }
                         <Collapsible trigger="Composer " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Only composer.json</label>
+                                <label><input type="checkbox" value="efda/php/composer/scopes" onChange={this.onChange.bind(this)}/>scopes</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With composer.lock </label>
+                                <label><input type="checkbox" value="efda/php/composer/with-composer.lock" onChange={this.onChange.bind(this)}/>with-composer.lock</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With Scopes (--no-dev) </label>
+                                <label><input type="checkbox" value="efda/php/composer/with-dev-dependencies" onChange={this.onChange.bind(this)}/>with-dev-dependencies</label>
                             </div>
                         </Collapsible>
                     </div>
@@ -199,40 +206,40 @@ class Home extends React.Component {
                 <Col md={3} mdPush={3}>
                     <h5 class="title"> 7) Golang </h5>
                     <div>  {/* Golang - Glide Projects */ }
-                        <Collapsible trigger="CoacoaPods " className="subtitle">
+                        <Collapsible trigger="Glide" className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Only glide.yaml</label>
+                                <label><input type="checkbox" value="efda/golang/glide/only-glide.yaml" onChange={this.onChange.bind(this)}/>only-glide.yaml</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With glde.lock </label>
+                                <label><input type="checkbox" value="efda/golang/glide/with-glide.lock" onChange={this.onChange.bind(this)}/>with-glde.lock</label>
                             </div>
                         </Collapsible>
                     </div>
                     <div>  {/* Golang - Trash Projects */ }
                         <Collapsible trigger="Trash " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With vendor.conf</label>
+                                <label><input type="checkbox" value="efda/golang/trash/trash-basic" onChange={this.onChange.bind(this)}/>trash-basic</label>
                             </div>
                         </Collapsible>
                     </div>
                     <div>  {/* Golang - Govendor Projects */ }
                         <Collapsible trigger="Govendor " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With vendor.json</label>
+                                <label><input type="checkbox" value="efda/golang/govendor/govendor-basic" onChange={this.onChange.bind(this)}/>govendor-basic</label>
                             </div>
                         </Collapsible>
                     </div>
                     <div>  {/* Golang - Godep Projects */ }
                         <Collapsible trigger="Godep " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With Godeps.json</label>
+                                <label><input type="checkbox" value="efda/golang/godep/godep-basic" onChange={this.onChange.bind(this)}/>godep-basic</label>
                             </div>
                         </Collapsible>
                     </div>
-                    <div>  {/* Golang - None Projects */ }
-                        <Collapsible trigger="None " className="subtitle">
+                    <div>  {/* Golang - No Package Manager Projects */ }
+                        <Collapsible trigger="No Package Manager" className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Using command "go get"</label>
+                                <label><input type="checkbox" value="efda/golang/no-package-manager/goget-basic" onChange={this.onChange.bind(this)}/>goget-basic</label>
                             </div>
                         </Collapsible>
                     </div>
@@ -242,13 +249,13 @@ class Home extends React.Component {
                         <div>  {/* Scala - SBT Projects */ }
                         <Collapsible trigger="SBT " className="subtitle">
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> With build.sbt</label>
+                                <label><input type="checkbox" value="efda/scala/sbt/multi-projects" onChange={this.onChange.bind(this)}/>multi-projects</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Version ranges</label>
+                                <label><input type="checkbox" value="efda/scala/sbt/sbt-basic" onChange={this.onChange.bind(this)}/>sbt-basic</label>
                             </div>
                             <div class="checkbox">
-                                <label><input type="checkbox" value=""/> Multi Modules</label>
+                                <label><input type="checkbox" value="efda/scala/sbt/version-ranges" onChange={this.onChange.bind(this)}/>version-ranges</label>
                             </div>
                         </Collapsible>
                     </div>
