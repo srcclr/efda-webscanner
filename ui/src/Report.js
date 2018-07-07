0e131498bdc1f5a5ef13c65d6555102a41772110
Terry Chia <terrycwk1994@gmail.com>
2018-07-07 16:05:26 +0800

    Format Report.js properly.

--- ui/src/Report.js
+++ ui/src/Report.js
@@ -20,72 +20,39 @@ class Report extends React.Component {
     }
 
     render() {
-  return (
-    <div>
-      <div className="container">
-        <h1 class="head">Report</h1>
-        <br />
-        <div class="row">
+        return (
+            <div>
+            <div className="container">
+            <h1 class="head">Report</h1>
+            <br />
+            <div class="row">
             <div class="col">
             <CollapsibleComponent>
-              <CollapsibleHead className="additionalClassForHead">Report 1: Project - </CollapsibleHead> {/* Number of panel based on number of projects*/ }
-                <CollapsibleContent class="additionalClassForContent">
+                <CollapsibleHead className="additionalClassForHead">Report 1: Project - </CollapsibleHead> {/* Number of panel based on number of projects*/ }
+                    <CollapsibleContent class="additionalClassForContent">
+
                     <ExpectedResultsJson />
 
-                  <div class="row">
-                    <div class="col">
-                      <SrcclrJson />
+                    <div class="row">
+                        <div class="col">
+                            <SrcclrJson />
+                        </div>
+                        <div class="col">
+                            <OWASPJson />
+                        </div>
                     </div>
-                    <div class="col">
-                      <OWASPJson />
-                    </div>
-                  </div>
-                </CollapsibleContent>
-            </CollapsibleComponent>
-            <br/>
-            <CollapsibleComponent>
-              <CollapsibleHead className="additionalClassForHead">Report 2: Project - </CollapsibleHead> {/* Number of panel based on number of projects*/ }
-                <CollapsibleContent className="additionalClassForContent">
-                <ExpectedResultsJson />
-                  <div class="row">
-                    <div class="col">
-                      <SrcclrJson />
-                    </div>
-                    <div class="col">
-                      <OWASPJson />
-                    </div>
-                  </div>
-                </CollapsibleContent>
-            </CollapsibleComponent>
-              <br/>
-            <CollapsibleComponent>
-              <CollapsibleHead className="additionalClassForHead">Report 3: Project - </CollapsibleHead> {/* Number of panel based on number of projects*/ }
-                <CollapsibleContent className="additionalClassForContent">
-              <ExpectedResultsJson />
-                  <div class="row">
-                    <div class="col">
-                      <SrcclrJson />
-                    </div>
-                    <div class="col">
-                      <OWASPJson />
-                    </div>
-                  </div>
                 </CollapsibleContent>
             </CollapsibleComponent>
             </div>
-
-
-        </div>
-        <br />
-        <p align="center">
-          <Link to="/"><Button bsStyle="primary">Back to scan</Button></Link> {/*Report page. Back to home page */ }
-        </p>
-
-      </div>
-    </div>
-  )
+            </div>
+            <br />
+            <p align="center">
+                <Link to="/"><Button bsStyle="primary">Back to scan</Button></Link> {/*Report page. Back to home page */ }
+            </p>
+            </div>
+            </div>
+        )
+    }
 }
-}
-
 
 export default Report;
